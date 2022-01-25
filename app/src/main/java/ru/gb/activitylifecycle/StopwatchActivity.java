ad299a8c88edfee371365c031c1f062a10396bea
Ser_Yo_Ja <seroja.92@mail.ru>
2022-01-25 16:39:06 +0300

    сборка метода runTimer(), определение работы метода

--- app/src/main/java/ru/gb/activitylifecycle/StopwatchActivity.java
+++ app/src/main/java/ru/gb/activitylifecycle/StopwatchActivity.java
@@ -3,9 +3,12 @@ package ru.gb.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class StopwatchActivity extends AppCompatActivity {
 
@@ -21,7 +24,8 @@ public class StopwatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
-        stopwatchTimer = findViewById(R.id.time_view);
+        runTimer();
+
         stopwatchStart = findViewById(R.id.button_start);
         stopwatchStop = findViewById(R.id.button_stop);
         stopwatchReset = findViewById(R.id.button_reset);
@@ -45,6 +49,25 @@ public class StopwatchActivity extends AppCompatActivity {
             public void onClick(View v) {
                 running = false;
                 seconds = 0;
+            }
+        });
+    }
+
+    private void runTimer(){
+        stopwatchTimer = findViewById(R.id.time_view);
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d",hours, minutes, secs);
+                stopwatchTimer.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
             }
         });
     }
